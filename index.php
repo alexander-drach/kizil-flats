2b143976e37b6e0222fc197b2344cc4e41d3a8c9
Alexander Drach <alexander-drach@mail.ru>
2023-11-15 10:56:10 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Кызыл квартиры посуточно</title>
+	<link rel="stylesheet" href="build/css/style.min.css">
+</head>
+
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="container">
+				<a class="logo logo--header" href="/">
+					<img src="build/img/svg/logo.svg" alt="Логотип Квартиры посуточно в Кызыле">
+				</a>
+			</div>
+		</header>
+		<main>
+			<div class="container">main</div>
+		</main>
+		<footer>
+			<div class="container">
+				<a class="logo logo--header" href="/">
+					<img src="build/img/svg/logo.svg" alt="Логотип Квартиры посуточно в Кызыле">
+				</a>
+			</div>
+		</footer>
+	</div>
+
+	<script src="js/vendor.min.js"></script>
+	<script src="js/main.min.js"></script>
+</body>
+
+</html>
